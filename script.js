36178b7ba5c15240692a7a063a639b0988b22879
Your Name Here <your_email@youremail.com>
2023-03-29 11:40:18 -0400

    Prompted the user for an input. Set up while loop with conditions to make sure the user does not input NaN or a negative number.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Chech the link in Console in Chrome Dev Tools
 // console.log("Hello World");
 
-let cat = "😸"
+let cat = "😸";
 
 // Guess That Number
 // Message to be used throughout the project
-const enterText = 'Please enter a number greater than zero:';
+const enterNumText = 'Please enter a number greater than zero:';
 
 // For restarting the game
-let restertGame = true
+let restartGame = true;
 
 // For sorting the range of the number to be guessed
 let rangeNum;
@@ -22,3 +22,26 @@ let guess;
 // For storing the users response to play again or not play again
 let playAgain;
 
+// Starting alert message
+alert('Welcome to "GUESS THAT NUMBER!" Please click "OK" to start the game');
+
+// Game restarts as long as restartGame has a value of true
+while (restartGame) {
+    // Asks the user to enter a number to set the upper bound for the random number that will be created.
+    rangeNum = prompt('Please enter a maximum number for the range');
+
+    // Using the parseInt to attempt to convert the user's response into a number value
+    rangeNum = parseInt(rangeNum)
+
+    // Verifies the user's entry for a range is a number greater than zero
+    // NOTE: All numbers, positive or negative, have a default boolean value of true, EXCEPT for zero, which has a default bollean value of false
+    while (!rangeNum || rangeNum < 1) {
+        rangeNum = propmt(enterNumText);
+        rangeNum = parseInt(rangeNum);
+    }
+
+
+
+
+    restartGame = false;
+};
