f07e2df3c601b72112991b2008132aaa9da281ec
Your Name Here <your_email@youremail.com>
2023-03-29 11:12:22 -0400

    Defined all variables to be used within project at this stage.

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 // Chech the link in Console in Chrome Dev Tools
 // console.log("Hello World");
 
+let cat = "😸"
+
 // Guess That Number
+// Message to be used throughout the project
+const enterText = 'Please enter a number greater than zero:';
+
+// For restarting the game
+let restertGame = true
+
+// For sorting the range of the number to be guessed
+let rangeNum;
+
+// For storing the number of attemps the user has left
+let lives;
+
+// For storing the user's guess
+let guess;
+
+// For storing the users response to play again or not play again
+let playAgain;
